ac251fad9c04bf19ad4123ac79e8348604ebb6d3
Artem Pereshein <pereshein.artem@gmail.com>
2021-11-02 23:59:12 +0300

    Url сервиса теперь берется из web.config.

--- GismeteoParser.WebClient/Global.asax.cs
+++ GismeteoParser.WebClient/Global.asax.cs
@@ -2,6 +2,7 @@
 using Autofac.Integration.Mvc;
 using GismeteoParser.ServiceClient;
 using System;
+using System.Configuration;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -13,9 +14,8 @@ namespace GismeteoParser.WebClient
         protected void Application_Start()
         {
             var builder = new ContainerBuilder();
-            //.WithParameter("serviceUri", new Uri("https://localhost:44361/"));
-            builder.Register(c => new WeatherClient(new Uri("https://localhost:44361/"))).As<IWeatherClient>();
-            //builder.RegisterType<WeatherClient>().As<IWeatherClient>()
+            builder.Register(c => new WeatherClient(
+                new Uri(ConfigurationManager.AppSettings["WebServiceUrl"]))).As<IWeatherClient>();
             builder.RegisterControllers(typeof(MvcApplication).Assembly);
             var container = builder.Build();
             DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
